e6bd20dbc0f439fc7ee671bba20c4f6dfa70d2ca
Rob Donnelly <rfdonnelly@gmail.com>
2020-06-05 14:18:50 -0700

    refactor: cli + lib + test

---
+++ lib/lfsr.rb
@@ -0,0 +1,59 @@
+def polynomial_to_bits(state_size, polynomial)
+  (0..state_size).map do |i|
+    mask = 1 << i
+    (polynomial & mask) > 0
+  end
+end
+
+def reduce(state_bits)
+  state_bits.map do |terms|
+    reduced_terms = []
+
+    uniq_terms = terms.sort.uniq
+    uniq_terms.each do |term|
+      term_count = terms.count(term)
+      if term_count % 2 == 1
+        reduced_terms.push(term)
+      end
+    end
+
+    reduced_terms
+  end
+end
+
+def state_to_s(state_bits)
+  state_bits.map.with_index do |terms, i|
+    'c[%d] = %s' % [i, terms.join(' ^ ')]
+  end
+    .join("\n")
+end
+
+def unroll_lfsr(data_size:, state_size:, polynomial:, has_variable_initial_state: false)
+  data_size -= 1
+  state_size -= 1
+  polynomial_bits = polynomial_to_bits(state_size, polynomial)
+
+  state_bits =
+    if has_variable_initial_state
+      (0..state_size).map { |i| ['is[%d]' % i] }
+    else
+      (0..state_size).map { [] }
+    end
+
+  (0..data_size).reverse_each do |data_bit_idx|
+    state_msb = state_bits[3]
+    data_bit = 'd[%d]' % data_bit_idx
+    feedback = state_msb.clone.push(data_bit)
+
+    (1..state_size).reverse_each do |state_bit_idx|
+      state_bits[state_bit_idx] = state_bits[state_bit_idx - 1]
+
+      if polynomial_bits[state_bit_idx]
+        state_bits[state_bit_idx].concat(feedback)
+      end
+    end
+    state_bits[0] = feedback
+  end
+
+  state_bits
+end
---
+++ test/test_lfsr.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class TestLFSR < Minitest::Test
+  def test_lfsr
+    state = unroll_lfsr(data_size: 8, state_size: 8, polynomial: 0x07)
+    state = reduce(state)
+    actual = state_to_s(state)
+    expected = <<~EOF.strip
+      c[0] = d[0] ^ d[2] ^ d[3] ^ d[7]
+      c[1] = d[0] ^ d[1] ^ d[2] ^ d[4] ^ d[7]
+      c[2] = d[0] ^ d[1] ^ d[5] ^ d[7]
+      c[3] = d[1] ^ d[2] ^ d[6]
+      c[4] = d[2] ^ d[3] ^ d[7]
+      c[5] = d[3] ^ d[4]
+      c[6] = d[4] ^ d[5]
+      c[7] = d[5] ^ d[6]
+    EOF
+    assert_equal expected, actual
+  end
+end
