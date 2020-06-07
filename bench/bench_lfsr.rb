67deb52ea6ff65bc828643e2958b1f99c05036ed
Rob Donnelly <rfdonnelly@gmail.com>
2020-06-07 16:34:31 -0700

    chore: fix whitespace

--- bench/bench_lfsr.rb
+++ bench/bench_lfsr.rb
@@ -5,7 +5,7 @@ include Benchmark
 
 Benchmark.benchmark(CAPTION, 8, FORMAT, '>total') do |x|
   state = []
-  
+
   t_unroll = x.report("unroll") do
     state = unroll_lfsr(data_size: 32, state_size: 8, polynomial: 0x07)
   end
