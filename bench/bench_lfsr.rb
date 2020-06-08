0bedaf4e3053a64118c2f95e3b52d37a4fd0fb44
Rob Donnelly <rfdonnelly@gmail.com>
2020-06-08 11:36:36 -0700

    test: add more intensive benchmarks
    now that we have good performance

--- bench/bench_lfsr.rb
+++ bench/bench_lfsr.rb
@@ -13,4 +13,10 @@ Benchmark.benchmark(CAPTION, 8, FORMAT, '>total') do |x|
   x.report("64 => 32") do
     unroll_lfsr(data_size: 64, state_size: 32, polynomial: 0x04C11DB7)
   end
+  x.report("256 => 32") do
+    unroll_lfsr(data_size: 256, state_size: 32, polynomial: 0x04C11DB7)
+  end
+  x.report("1024 => 32") do
+    unroll_lfsr(data_size: 1024, state_size: 32, polynomial: 0x04C11DB7)
+  end
 end
